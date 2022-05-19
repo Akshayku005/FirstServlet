357eb4a4b1b350459574b2a71a685fe62d4f371d
Akshayku005 <99657121+Akshayku005@users.noreply.github.com>
2022-05-19 09:05:08 +0530

    Update LoginServlet.java

--- src/main/java/com/bridgeLabzs/LoginServlet.java
+++ src/main/java/com/bridgeLabzs/LoginServlet.java
@@ -1,39 +1,44 @@
 package com.bridgeLabzs;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
-@WebServlet("/LoginServlet")
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = {
+        		@WebInitParam(name = "user", value = "Akshay"),
+        		@WebInitParam  (name = "password", value = "Akshay005")
+        }
+)
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-	
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		   String user = req.getParameter("user");
-	        String pwd = req.getParameter("pwd");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
 
 
-    String userID = getServletConfig().getInitParameter("user");
-    String password = getServletConfig().getInitParameter("password");
-    if(userID.equals(user) && password.equals(pwd)) {
-        req.setAttribute("user",user);
-        req.getRequestDispatcher("LoginSuccess.jsp").forward(req,res);
-    } else {
-        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-        PrintWriter out  = res.getWriter();
-        out.println("<font color = red> Either username or password is wrong</font>");
-        rd.include(req, res);
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        if(userID.equals(user) && password.equals(pwd)) {
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,resp);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out  = resp.getWriter();
+            out.println("<font color = red> Either username or password is wrong</font>");
+            rd.include(req, resp);
+        }
     }
 }
-	}
 
 
